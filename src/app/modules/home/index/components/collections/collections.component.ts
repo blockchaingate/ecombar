064923ed06658736343d8ec893dc5f25c8ba68cf
ken <kenin.qiu@gmail.com>
2020-07-10 15:06:56 -0400

    add layout

---
+++ src/app/modules/home/index/components/collections/collections.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collections',
+  templateUrl: './collections.component.html',
+  styleUrls: ['./collections.component.scss']
+})
+export class CollectionsComponent {
+
+}
--- src/app/modules/home/index/index.module.ts
+++ src/app/modules/home/index/index.module.ts
@@ -3,11 +3,13 @@ import { IndexRoutingModule } from './index-routing.module';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
 import { SlideShowComponent } from './components/slide-show/slide-show.component';
+import { CollectionsComponent } from './components/collections/collections.component';
 
 @NgModule({
   declarations: [
     IndexComponent,
-    SlideShowComponent
+    SlideShowComponent,
+    CollectionsComponent
   ],
   imports: [
     IndexRoutingModule,
