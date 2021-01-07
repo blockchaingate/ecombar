7c3079ed505d585c65da5963125a44fda755d628
ken <kenin.qiu@gmail.com>
2021-01-06 20:16:14 -0500

    f

--- src/app/modules/home/category2/category2.component.ts
+++ src/app/modules/home/category2/category2.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../shared/services/category.service';
-import {ActivatedRoute, ParamMap} from '@angular/router';
+import {ActivatedRoute, Router, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-category2',
@@ -10,9 +10,10 @@ import {ActivatedRoute, ParamMap} from '@angular/router';
 export class Category2Component implements OnInit {
   categories: any;
   category: any;
+  currentCatorory: any;
   categoryChildren: any;
   id: string;
-  constructor(private route:ActivatedRoute, private categoryServ: CategoryService) { }
+  constructor(private router: Router, private route:ActivatedRoute, private categoryServ: CategoryService) { }
 
   ngOnInit() {
     this.categoryServ.getAdminCategoriesWithCount().subscribe(
@@ -27,6 +28,7 @@ export class Category2Component implements OnInit {
           this.route.paramMap.subscribe((params: ParamMap) =>  {
             this.id = params.get('id');
             this.category = body.filter(item => item._id === this.id)[0];
+            this.currentCatorory = this.category;
             console.log('this.category=', this.category);
             if(this.category.parentId) {
               this.category = body.filter(item => item._id === this.category.parentId)[0];
@@ -39,6 +41,10 @@ export class Category2Component implements OnInit {
       }
     );
 
+  }
+
+  navigateTo(category) {
+    this.router.navigate(['/category/' + category._id]);
   }
 
 }
