63cb131db37dabee4ec92a928d99213870a82176
ken <kenin.qiu@gmail.com>
2021-03-17 21:56:16 -0400

    fi

---
+++ src/app/modules/nft/components/collections-header/collections-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    providers: [],
+    selector: 'app-nft-collections-header',
+    templateUrl: './collections-header.component.html',
+    styleUrls: ['./collections-header.component.scss']
+  })
+  export class NftCollectionsHeaderComponent implements OnInit {
+      ngOnInit() {
+          
+      }
+  }
--- src/app/modules/nft/nft.module.ts
+++ src/app/modules/nft/nft.module.ts
@@ -3,14 +3,18 @@ import { NftRoutingModule } from './nft-routing.module';
 import { NftComponent } from './nft.component';
 import { NftHeaderComponent } from './components/header/header.component';
 import { NftAdminComponent } from './components/admin/admin.component';
+import { NftAdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { NftCollectionsComponent } from './components/collections/collections.component';
+import { NftCollectionsHeaderComponent } from './components/collections-header/collections-header.component';
 
 @NgModule({
     declarations: [
       NftComponent,
       NftHeaderComponent,
       NftAdminComponent,
-      NftCollectionsComponent
+      NftAdminSidebarComponent,
+      NftCollectionsComponent,
+      NftCollectionsHeaderComponent
     ],
     imports: [
         NftRoutingModule
