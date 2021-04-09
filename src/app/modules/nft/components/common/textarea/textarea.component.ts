30ad03245a951362c03180a7cff5e1717405fcfe
ken <kenin.qiu@gmail.com>
2021-04-08 22:56:12 -0400

    f

--- src/app/modules/nft/components/common/textarea/textarea.component.ts
+++ src/app/modules/nft/components/common/textarea/textarea.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
-    providers: [],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => TextareaComponent),
+        multi: true
+    }],
     selector: 'app-textarea',
     templateUrl: './textarea.component.html',
     styleUrls: ['./textarea.component.scss']
 })
-export class TextareaComponent implements OnInit {
+export class TextareaComponent implements ControlValueAccessor, OnInit {
     name: string;
     @Input() title: string;
     @Input() subtitle: string;
@@ -14,4 +19,58 @@ export class TextareaComponent implements OnInit {
     ngOnInit() {
           
     }
+
+
+ /**
+  * Holds the current value of the slider
+  */
+ value: number = 0;
+
+ /**
+  * Invoked when the model has been changed
+  */
+ onChange: (_: any) => void = (_: any) => {};
+
+ /**
+  * Invoked when the model has been touched
+  */
+ onTouched: () => void = () => {};
+
+ constructor() {}
+
+ /**
+  * Method that is invoked on an update of a model.
+  */
+ updateChanges() {
+     this.onChange(this.value);
+ }
+
+ ///////////////
+ // OVERRIDES //
+ ///////////////
+
+ /**
+  * Writes a new item to the element.
+  * @param value the value
+  */
+ writeValue(value: number): void {
+     this.value = value;
+     this.updateChanges();
+ }
+
+ /**
+  * Registers a callback function that should be called when the control's value changes in the UI.
+  * @param fn
+  */
+ registerOnChange(fn: any): void {
+     this.onChange = fn;
+ }
+
+ /**
+  * Registers a callback function that should be called when the control receives a blur event.
+  * @param fn
+  */
+ registerOnTouched(fn: any): void {
+     this.onTouched = fn;
+ }    
 }
--- src/app/modules/nft/components/common/textbox/textbox.component.ts
+++ src/app/modules/nft/components/common/textbox/textbox.component.ts
@@ -1,27 +1,74 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
-    providers: [],
     selector: 'app-textbox',
     templateUrl: './textbox.component.html',
-    styleUrls: ['./textbox.component.scss']
+    styleUrls: ['./textbox.component.scss'],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => TextboxComponent),
+        multi: true
+    }]    
 })
-export class TextboxComponent implements OnInit {
-    /**
-     * Holds the current value of the slider
-     */
-    @Input() inputValue: string = "";
-
-    /**
-     * Invoked when the model has been changed
-     */
-    @Output() inputValueChange: EventEmitter<string> = new EventEmitter<string>();
-
-
+export class TextboxComponent implements ControlValueAccessor, OnInit {
+    
     @Input() title: string;
     @Input() subtitle: string;
     @Input() placeholder: string;
 
+ /**
+  * Holds the current value of the slider
+  */
+ value: number = 0;
+
+ /**
+  * Invoked when the model has been changed
+  */
+ onChange: (_: any) => void = (_: any) => {};
+
+ /**
+  * Invoked when the model has been touched
+  */
+ onTouched: () => void = () => {};
+
+ constructor() {}
+
+ /**
+  * Method that is invoked on an update of a model.
+  */
+ updateChanges() {
+     this.onChange(this.value);
+ }
+
+ ///////////////
+ // OVERRIDES //
+ ///////////////
+
+ /**
+  * Writes a new item to the element.
+  * @param value the value
+  */
+ writeValue(value: number): void {
+     this.value = value;
+     this.updateChanges();
+ }
+
+ /**
+  * Registers a callback function that should be called when the control's value changes in the UI.
+  * @param fn
+  */
+ registerOnChange(fn: any): void {
+     this.onChange = fn;
+ }
+
+ /**
+  * Registers a callback function that should be called when the control receives a blur event.
+  * @param fn
+  */
+ registerOnTouched(fn: any): void {
+     this.onTouched = fn;
+ }
     
     ngOnInit() {
           
