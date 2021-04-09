e41108987c01206483eaf3badea5c755e14e1275
ken <kenin.qiu@gmail.com>
2021-04-09 09:45:44 -0400

    f

--- src/app/modules/nft/components/common/image-uploader/image-uploader.component.ts
+++ src/app/modules/nft/components/common/image-uploader/image-uploader.component.ts
@@ -9,7 +9,9 @@ import { Component, OnInit, Input } from '@angular/core';
   export class ImageUploaderComponent implements OnInit {
     @Input() title: string;
     @Input() subtitle: string;
+    image: string;
       ngOnInit() {
-          
+        //this.image = "blob:https://testnets.opensea.io/bda6b063-8f9c-495c-86d4-76260b42c86f";
+          //this.image = 'https://lh3.googleusercontent.com/E_vznLJI8etM3V_AmkgNeaIFDZ1ve4v9w-IBMkU4BzgisoX4kptb3jiD0fJcJpQoNwjrEmD61sA8cWVw9GDXIN7MlvVnjkUEcGCU=w250';
       }
   }
