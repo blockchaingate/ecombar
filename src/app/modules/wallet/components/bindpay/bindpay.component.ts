a341938c1c0fc70e912a20b15177c2e5d6b1076b
Ken Qiu <kenin.qiu@gmail.com>
2021-07-22 15:59:18 -0400

    fox

--- src/app/modules/wallet/components/bindpay/bindpay.component.ts
+++ src/app/modules/wallet/components/bindpay/bindpay.component.ts
@@ -7,6 +7,8 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { KanbanSmartContractService } from 'src/app/modules/shared/services/kanban.smartcontract.service';
 import { ToastrService } from 'ngx-toastr';
 import { Web3Service } from 'src/app/modules/shared/services/web3.service';
+import { StarService } from 'src/app/modules/shared/services/star.service';
+import { UtilService } from 'src/app/modules/shared/services/util.service';
 
 @Component({
   selector: 'app-wallet-bindpay',
@@ -20,16 +22,21 @@ export class BindpayComponent implements OnInit{
     name: string;
     to: string;
     data: string;
+    args: any;
+    parents: string[];
 
     constructor(    
         private kanbanSmartContractServ: KanbanSmartContractService,
         private dataServ: DataService,
         private route: ActivatedRoute,
         private toastr: ToastrService,
+        private starServ: StarService,
         private web3Serv: Web3Service,
+        private utilServ: UtilService,
         public kanbanServ: KanbanService,
         private modalService: BsModalService,) {}
     ngOnInit() {
+        this.parents = [];
         this.dataServ.currentWallet.subscribe(
             (wallet: string) => {
               this.wallet = wallet;
@@ -40,9 +47,23 @@ export class BindpayComponent implements OnInit{
             this.name = params['name'];
             this.to = params['to'];
             this.data = params['data'];
-            const decoded = this.web3Serv.decodeData(['bytes32', 'uint32', 'uint256', 'address[]'], this.data.substring(10));
-            console.log('decoded=', decoded);
+            this.args = this.web3Serv.decodeData(['bytes32', 'uint32', 'uint256', 'address[]'], this.data.substring(10));
+            console.log('decoded=', this.args);
         });
+
+        this.dataServ.currentWalletAddress.subscribe(
+            (walletAddress: string) => {
+                if(walletAddress) {
+                    this.starServ.getParents(walletAddress).subscribe(
+                        (ret: any) => {
+                            console.log('ret for getParents=', ret);
+                            this.parents = ret.map(item => this.utilServ.fabToExgAddress(item));
+                        }
+                    );
+                }
+            }
+        );
+        
     }
 
     submit() {
@@ -59,7 +80,49 @@ export class BindpayComponent implements OnInit{
     }
 
     async submitDo(seed: Buffer) {
-        const ret = await this.kanbanSmartContractServ.execSmartContractWithData(seed, this.to, this.data);
+        const abi = {
+            "constant": false,
+            "inputs": [
+              {
+                "name": "_orderID",
+                "type": "bytes32"
+              },
+              {
+                "name": "_coinType",
+                "type": "uint32"
+              },
+              {
+                "name": "_totalAmount",
+                "type": "uint256"
+              },
+              {
+                "name": "_rewardBeneficiary",
+                "type": "address[]"
+              }
+            ],
+            "name": "chargeFundsWithFee",
+            "outputs": [
+              {
+                "name": "",
+                "type": "bool"
+              }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+          };
+
+        this.args[3] = this.parents;
+        
+        const args = [
+            this.args[0],
+            this.args[1],
+            this.args[2],
+            this.args[3]
+        ];
+
+        console.log('argsss=', args);
+        const ret = await this.kanbanSmartContractServ.execSmartContract(seed, this.to, abi, args);
         if(ret && ret.ok && ret._body && ret._body.status == '0x1') {
             this.toastr.success('the transaction was procssed successfully');
         } else {
