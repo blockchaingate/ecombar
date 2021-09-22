e0d4b1dcbdf617091cda1a8294d554165207e793
Paul Liu <bitspadorg@gmail.com>
2021-09-22 18:44:06 -0400

    update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ecombar app is running!');
+    expect(page.getTitleText()).toEqual('madearn app is running!');
   });
 
   afterEach(async () => {
