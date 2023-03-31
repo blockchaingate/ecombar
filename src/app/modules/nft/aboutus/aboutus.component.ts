1f834af87bb36f29fa71776b8f8b4342647c3311
Ken Qiu <kenin.qiu@gmail.com>
2023-03-31 07:48:19 -0400

    added nft type description and about us page

---
+++ src/app/modules/nft/aboutus/aboutus.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './aboutus.component.html',
+  styleUrls: ['./aboutus.component.css']
+})
+export class AboutUsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
