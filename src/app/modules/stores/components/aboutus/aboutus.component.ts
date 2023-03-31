888dc213836f6259928613f5a8c46d86ae2598fe
Ken Qiu <kenin.qiu@gmail.com>
2023-03-30 21:42:55 -0400

    added about us page

---
+++ src/app/modules/stores/components/aboutus/aboutus.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './aboutus.component.html',
+  styleUrls: ['./aboutus.component.css']
+})
+export class AboutUsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
