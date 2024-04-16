93b63a6c76a6c9cb49d858038b9b0262aac23616
Paul Liu <paullby@gmail.com>
2024-04-16 17:53:58 -0400

    update

--- build-prod.sh
+++ build-prod.sh
@@ -1,1 +1,14 @@
+#node patch  if some error, run node patch after npm i
+
+# To reslove Error: error:0308010C:digital envelope routines::unsupported:#-
+# On Unix-like (Linux, macOS, Git bash, etc.):                            #-
+export NODE_OPTIONS=--openssl-legacy-provider                             #-
+                                                                          #-
+# On Windows command prompt:                                              #-
+set NODE_OPTIONS=--openssl-legacy-provider                                #-
+                                                                          #-
+#On PowerShell:                                                           #-
+$env:NODE_OPTIONS = "--openssl-legacy-provider"                           #-
+#-------------------------------------------------------------------------#-
+
 ng build --prod=true --aot=true --optimization=true
